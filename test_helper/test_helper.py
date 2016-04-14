d180945e2db9610866709ec953b4ad9eb3f87368
vladimirfr <vladimirfrolenko@gmail.com>
2016-04-14 20:55:04 +0300

    Update test_helper.py

--- test_helper/test_helper.py
+++ test_helper/test_helper.py
@@ -250,7 +250,7 @@ class Test(object):
     def uniqueRecord(cls, data, client, msg="", msg_success=""):
         result = False
         if(client.twitter.tweets.distinct('id')==client.twitter.tweets.count() and client.twitter.users.distinct('id')==client.twitter.users.count()):
-        result = True
+			result = True
         cls.assertEquals(True, result, msg, msg_success)
     # Lab 8.2 Ex.5.5
     @classmethod
